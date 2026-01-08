28222fa0a2f3a126123f58c7ef27a6382bc97a71
AriadnaVND <victoria06nolasco@gmail.com>
2026-01-08 10:45:54 -0500

    .

--- backendp/routes/sunat.js
+++
@@ -1,9 +0,0 @@
-// routes/sunat.js
-const express = require('express');
-const router = express.Router();
-const { consultarRUC, consultarDNI } = require('../controllers/sunatController');
-
-router.post('/consultar-ruc', consultarRUC);
-router.post('/consultar-dni', consultarDNI);
-
-module.exports = router;
